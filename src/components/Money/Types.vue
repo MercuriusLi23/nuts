36810392857250d016109ba42328a0fcef6a7d95
liersan <1031121541@qq.com>
2022-12-16 19:54:57 +0800

    完成Monry页面模块化

---
+++ src/components/Money/Types.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <ul class="types">
+      <li class="selected">支出</li>
+      <li>收入</li>
+    </ul>
+  </div>
+</template>
+
+<script lang='ts'>
+export default {
+  name: 'Types'
+};
+</script>
+
+<style lang='scss' scoped>
+.types{
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  > li{
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected::after{
+      content: '';
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      height: 4px;
+      border-bottom: 4px solid #333333;
+    }
+  }
+}
+</style>
