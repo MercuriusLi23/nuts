cf997ed8e5af69a62fe4d0aafc8aa4a42864ea6f
liersan <1031121541@qq.com>
2022-12-17 19:13:31 +0800

    完成tag的选中和取消功能

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,18 +4,31 @@
       <button>新增标签</button>
     </div>
     <ul class="current">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
+      <li v-for="tag in dataSource" :key="tag"
+          :class="{selected: selectedTags.indexOf(tag) >= 0}"
+      @click="toggle(tag)">{{tag}}</li>
     </ul>
   </div>
 </template>
 
 <script lang='ts'>
-export default {
-  name: 'Tags'
-};
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+@Component
+export default class Tags extends Vue {
+@Prop() dataSource: string[] | undefined;  //注明为字符串数组,默认为空
+selectedTags: string[] = [];
+
+toggle(tag: string){
+  const index = this.selectedTags.indexOf(tag);
+  if(index>=0){
+this.selectedTags.splice(index,1)
+  }else{
+    this.selectedTags.push(tag);
+  }
+}
+  }
+
 </script>
 
 <style lang='scss' scoped>
@@ -37,6 +50,10 @@ export default {
       padding: 0 16px;
       margin-right:12px;
       margin-top: 4px;
+      &.selected {
+        background: #daa520;
+        color:white;
+      }
     }
   }
   > .new{
